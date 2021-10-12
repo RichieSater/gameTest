413389f3ccb63adacffa93481032b008e8ee92a6
Richie Sater <richiesater@gmail.com>
2021-10-12 09:11:39 -0400

    Added some tests to the scratchpad around sound, user input, text interactivity, and graphic generation - content with tests and using phaser - next will start building pieces for actual game in toolbox - going to develop a couple tutorial games before starting the actual cryptogame

--- scratchPad/js/sceneMain.js
+++ scratchPad/js/sceneMain.js
@@ -7,14 +7,25 @@ class SceneMain extends Phaser.Scene {
         //load our images or sounds
         //this.load.image("key","path")
     	this.load.spritesheet('boy', 'images/boy.png', { frameWidth: 120, frameHeight: 200 });
+        this.load.audio('cat',['audio/meow.mp3','audio/meow.ogg'])
     }
     create() {
         //define our objects
 
+        //Cat
+        this.catSound=this.sound.add('cat');
+        this.catSound.play()
+        
+        //Interactive Text
         this.text1=this.add.text(game.config.width/2,game.config.height/2,"HELLO!",{fontFamily:'Anton',color:'#ff0000',fontSize:40});
         this.text1.setOrigin(0.5,0.5)
+        this.text1.setInteractive();
+        this.text1.on('pointerdown', this.onDown,this);
+        this.text1.on('pointerup', this.onUp,this);
         this.char = this.add.sprite(game.config.width/2,game.config.height/2,'boy');
 
+
+        //Kyle Animation
         this.anims.create({
             key: 'walk',
             frames: [
@@ -29,11 +40,23 @@ class SceneMain extends Phaser.Scene {
             frameRate:8,
             repeat:-1
         })
-
         this.char.play('walk')
         this.doWalk()
         
-        
+        //Phaser Graphics and catSound
+        this.graphics = this.add.graphics();
+        this.graphics.lineStyle(8,0xff0000)
+        this.graphics.strokeRect(100,200,50,50)
+        this.graphics.strokePath();
+
+    }
+
+    onDown(){
+        this.text1.alpha=.1;
+    }
+
+    onUp(){
+        this.text1.alpha=1;
     }
 
     doWalk(){
