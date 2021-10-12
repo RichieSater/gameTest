3495f9b65f4d64c4d95dfb71d487a37099f0d906
Richie Sater <richiesater@gmail.com>
2021-10-12 08:51:27 -0400

    added font-loader from Google Fonts to basicTemplate, updated animation with Tweens Targetting

--- scratchPad/js/sceneMain.js
+++ scratchPad/js/sceneMain.js
@@ -10,6 +10,9 @@ class SceneMain extends Phaser.Scene {
     }
     create() {
         //define our objects
+
+        this.text1=this.add.text(game.config.width/2,game.config.height/2,"HELLO!",{fontFamily:'Anton',color:'#ff0000',fontSize:40});
+        this.text1.setOrigin(0.5,0.5)
         this.char = this.add.sprite(game.config.width/2,game.config.height/2,'boy');
 
         this.anims.create({
@@ -28,9 +31,35 @@ class SceneMain extends Phaser.Scene {
         })
 
         this.char.play('walk')
+        this.doWalk()
+        
+        
+    }
+
+    doWalk(){
+        this.tweens.add({
+            targets: this.char,
+            duration: 1000,
+            x:game.config.width,
+             y:0, 
+             alpha:0,
+             onComplete: this.onCompleteHandler,
+            onCompleteParams:[this]});
+    }
+
+    onCompleteHandler (tween, targets, scope) {
+        var char=targets[0]
+        char.x=0;
+        char.alpha=1;
+        char.y=game.config.height/2;
+        scope.doWalk()
+
+
+
     }
 
     update() {
         //constant running loop
+        
     }
 }
